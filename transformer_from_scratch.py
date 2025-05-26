bf517e5ec35d48645d75e64190819e6ca296c688
Balles18 <ballestero98@gmail.com>
2025-05-26 17:18:06 -0600

    Pytoch-Create Word Embedding

--- transformer_from_scratch.py
+++ transformer_from_scratch.py
@@ -9,4 +9,11 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 warnings.filterwarnings("ignore")
-print(torch.__version__)
+
+class Embedding(nn.Module):
+    def __init__(self, vocab_size, embed_dim):
+        super(Embedding, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_dim)
+    def forward(self, x):
+        out = self.embed(x)
+        return out
