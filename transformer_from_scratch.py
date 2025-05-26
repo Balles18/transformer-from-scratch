41c726cae5a7ba55862a9ca213b51636705897c8
Balles18 <ballestero98@gmail.com>
2025-05-26 17:06:05 -0600

    Pytoch-Create Word Embedding

---
+++ transformer_from_scratch.py
@@ -0,0 +1,12 @@
+#importing necessary libraries
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math,copy,re
+import warnings
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore")
+print(torch.__version__)
