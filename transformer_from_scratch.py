365aaf8ad62470a25da0ccac1a6adcdc1a5a8f80
Balles18 <ballestero98@gmail.com>
2025-05-27 15:43:58 -0600

    Pytorch-Multihead Attention

--- transformer_from_scratch.py
+++ transformer_from_scratch.py
@@ -38,3 +38,61 @@ class PositioanlEmbedding(nn.Module):
             x = x + torch.autograd.Variable(self.pe[:, :seq_len], requires_grad=False)
             return x
         
+class MultiheadAttention(nn.Module):
+    def __init__(self, embed_dim = 512, n_heads = 8):
+        super(MultiheadAttention, self).__init__()
+        self.embed_dim = embed_dim
+        self.n_heads = n_heads
+        self.single_head_dim = int(self.embed_dim / self.n_heads)
+
+        self.query_matrix = nn.Linear(self.single_head_dim, self.single_head_dim, bias=False)
+        self.key_matrix = nn.Linear(self.single_head_dim, self.single_head_dim, bias=False) 
+        self.value_matrix = nn.Linear(self.single_head_dim, self.single_head_dim, bias=False)  
+
+        self.out = nn.Linear(self.n_heads * self.single_head_dim, self.embed_dim)
+
+        def forward(self, query, key, value, mask=None): 
+            batch_size = key.size(0)
+            seq_len = key.size(1)
+
+            seq_len_query = query.size(1)
+
+            #32*10*512 -> 32*10*8*512
+
+            key = key.view(batch_size, seq_len, self.n_heads, self.single_head_dim)
+
+            query = query.view(batch_size, seq_len, self.n_heads, self.single_head_dim)
+
+            value = value.view(batch_size, seq_len, self.n_heads, self.single_head_dim)
+
+            k = self.key_matrix(key)
+            q = self.query_matrix(query)
+            v = self.value_matrix(value)
+
+            #32*10*8*512 -> 32*8*10*512
+
+            k = k.transpose(1, 2)
+            q = q.transpose(1, 2)       
+            v = v.transpose(1, 2)
+
+            k_adjusted = k.trasnpose(-1, -2)
+            product = torch.matmul(q, k_adjusted)
+
+            if mask is not None:
+                product = product.masked_fill(mask == 0, float('-1e9'))
+            
+            product = product / math.sqrt(self.single_head_dim)
+
+            scores = F.softmax(product, dim=-1)
+
+            scores = torch.matmul(scores, v)
+
+            concat = scores.transpose(1, 2).contiguous().view(batch_size, seq_len_query, self.n_heads * self.single_head_dim)
+
+            #32*8*10*64 -> 32*10*8*64 ->32*10*512
+
+            out = self.out(concat) # 32*10*512
+
+            return out
+
+
