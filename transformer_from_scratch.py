a0afcceff2bf1e4ee63a967e159af2569299cb6a
Balles18 <ballestero98@gmail.com>
2025-05-27 16:54:39 -0600

    Pytorch-Transformer Decoder

--- transformer_from_scratch.py
+++ transformer_from_scratch.py
@@ -18,9 +18,9 @@ class Embedding(nn.Module):
         out = self.embed(x)
         return out
 
-class PositioanlEmbedding(nn.Module):
+class PositionalEmbedding(nn.Module):
     def __init__(self, max_seq_len, embed_model_dim):
-        super(PositioanlEmbedding, self).__init__()
+        super(PositionalEmbedding, self).__init__()
         self.embed_dim = embed_model_dim
         
         pe = torch.zeros(max_seq_len, self.embed_dim)
@@ -125,3 +125,57 @@ class TransformerBlock(nn.Module):
         norm2_out = self.dropout2(self.norm2(feed_fwd_residual_out)) # 32*10*512
 
         return norm2_out
+
+class DecoderBlock(nn.Module):
+    def __init__(self, embed_dim, expansion_factor=4, n_heads=8):
+        super(DecoderBlock, self).__init__()
+
+        self.attention = MultiheadAttention(embed_dim, n_heads=8)
+        self.norm = nn.LayerNorm(embed_dim)
+        self.dropout = nn.Dropout(0.2)
+        self.transformer_block = TransformerBlock(embed_dim, expansion_factor, n_heads)
+
+
+    def forward(self, key, query, x,mask):
+
+        #we need to pass mask mask only to fst attention
+        attention = self.attention(x,x,x,mask=mask) #32x10x512
+        value = self.dropout(self.norm(attention + x))
+
+        out = self.transformer_block(key, query, value)
+
+
+        return out
+
+
+class TransformerDecoder(nn.Module):
+    def __init__(self, target_vocab_size, embed_dim, seq_len, num_layers=2, expansion_factor=4, n_heads=8):
+        super(TransformerDecoder, self).__init__()
+  
+        self.word_embedding = nn.Embedding(target_vocab_size, embed_dim)
+        self.position_embedding = PositionalEmbedding(seq_len, embed_dim)
+
+        self.layers = nn.ModuleList(
+            [
+                DecoderBlock(embed_dim, expansion_factor=4, n_heads=8)
+                for _ in range(num_layers)
+            ]
+
+        )
+        self.fc_out = nn.Linear(embed_dim, target_vocab_size)
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, x, enc_out, mask):
+
+
+
+        x = self.word_embedding(x)  #32x10x512
+        x = self.position_embedding(x) #32x10x512
+        x = self.dropout(x)
+
+        for layer in self.layers:
+            x = layer(enc_out, x, enc_out, mask)
+
+        out = F.softmax(self.fc_out(x))
+
+        return out
